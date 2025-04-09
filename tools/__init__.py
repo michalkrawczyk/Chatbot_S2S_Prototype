9869adef0976f9865e9345e667aa9d79cb8d8fef
TK <mkrwczyk.1@gmail.com>
2025-04-10 00:31:29 +0200

    Fixing tool node

--- tools/__init__.py
+++ tools/__init__.py
@@ -1,4 +1,4 @@
-from tools.tool_register import DEFINED_TOOLS_DICT, DEFINED_TOOLS
+from tools.tool_register import DEFINED_TOOLS_DICT, DEFINED_TOOLS, DEFINED_TOOL_NODE
 
 
-__all__ = ["DEFINED_TOOLS_DICT", "DEFINED_TOOLS"]
+__all__ = ["DEFINED_TOOLS_DICT", "DEFINED_TOOLS", "DEFINED_TOOL_NODE"]
