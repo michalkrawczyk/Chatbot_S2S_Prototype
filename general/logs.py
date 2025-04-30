bae200df4fa0c852303b749331db41d05993326b
mk-tai <mkrwczyk.1@gmail.com>
2025-04-30 21:04:39 +0200

    black general

--- general/logs.py
+++ general/logs.py
@@ -2,16 +2,16 @@ import logging
 
 from general.config import ADDITIONAL_LOGGER_INFO
 
-logger = logging.getLogger('voice_transcriber')
+logger = logging.getLogger("voice_transcriber")
 
 # Set up logging
 logging.basicConfig(
-    level=logging.INFO,
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+    level=logging.INFO, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
 )
 
+
 def conditional_logger_info(*args, **kwargs):
-    """ Log messages conditionally based on ADDITIONAL_LOGGER_INFO setting
+    """Log messages conditionally based on ADDITIONAL_LOGGER_INFO setting
     Calling just them on debug would be too verbose (because of information from any other functions)
     This is simple wrapper to avoid writing each time new logger
     """
